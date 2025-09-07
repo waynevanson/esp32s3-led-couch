396fdb2b01cfd9c355d7472e5f78d6d1fbf6da9c
Wayne Van Son <waynevanson@gmail.com>
2025-09-07 22:14:09 +1000

    feat!: cargo build works

---
+++ flake.nix
@@ -0,0 +1,66 @@
+# 1. Use pre-built binary of rust via `esp-rs`
+#    https://github.com/esp-rs/rust-build/releases/tag/v1.88.0.0
+# 2. Install LLVM fork + GCC toolchain (crosstool-ng) for build time.
+#
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?rev=d0907b75146a0ccc1ec0d6c3db287ec287588ef6";
+    flake-utils.url = "github:numtide/flake-utils";
+    esp-dev-rust.url = "github:waynevanson/nixpkgs-esp-dev-rust";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    esp-dev-rust,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [esp-dev-rust.overlays.default];
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            esp-idf-full
+
+            # Tools required to use ESP-IDF.
+            git
+            wget
+            gnumake
+
+            flex
+            bison
+            gperf
+            pkg-config
+            cargo-generate
+
+            cmake
+            ninja
+
+            ncurses5
+
+            llvm-xtensa
+            llvm-xtensa-lib
+            rust-xtensa
+
+            ldproxy
+            espflash
+
+            python3
+            python3Packages.pip
+            python3Packages.virtualenv
+          ];
+          shellHook = ''
+            # fixes libstdc++ issues and libgl.so issues
+            export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [pkgs.libxml2 pkgs.zlib pkgs.stdenv.cc.cc.lib]}
+            export ESP_IDF_VERSION=v5.3
+            export LIBCLANG_PATH=${pkgs.llvm-xtensa-lib}/lib
+            export RUSTFLAGS="--cfg espidf_time64"
+          '';
+        };
+      }
+    );
+}
---
+++ src/bin/main.rs
@@ -0,0 +1,43 @@
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+
+use esp_hal::clock::CpuClock;
+use esp_hal::main;
+use esp_hal::time::{Duration, Instant};
+use esp_hal::timer::timg::TimerGroup;
+
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+// This creates a default app-descriptor required by the esp-idf bootloader.
+// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
+esp_bootloader_esp_idf::esp_app_desc!();
+
+// write white light to the LED's.
+
+#[main]
+fn main() -> ! {
+    // generator version: 0.5.0
+
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    esp_alloc::heap_allocator!(size: 64 * 1024);
+
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    let _init = esp_wifi::init(timg0.timer0, esp_hal::rng::Rng::new(peripherals.RNG)).unwrap();
+
+    loop {
+        let delay_start = Instant::now();
+        while delay_start.elapsed() < Duration::from_millis(500) {}
+    }
+
+    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-rc.0/examples/src/bin
+}
